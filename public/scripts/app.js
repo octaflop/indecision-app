c1d794bfdf0451f9304fef03f20cfd8a8fdf3662
Faris Chebib <faris@theluckybead.com>
2017-12-26 16:50:35 -0700

    Complete challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,19 +20,38 @@ var user = {
   printPlacesLived: function printPlacesLived() {
     var _this = this;
 
-    console.log(this.name);
-    console.log(this.cities);
-
-    this.cities.forEach(function (city) {
-      // this is no longer bound in the loop!
-      // usually we do: const that = this above
-      // console.log(this.name + ' has lived in ' + city);
+    return this.cities.map(function (city) {
+      return _this.name + ' has lived in ' + city;
     });
+    // this.cities.forEach(function (city){
+    // this is no longer bound in the loop!
+    // usually we do: const that = this above
+    // console.log(this.name + ' has lived in ' + city);
+    // });
     // arrow function does work
-    this.cities.forEach(function (city) {
-      console.log(_this.name + ' has lived in ' + city);
+    // this.cities.forEach((city) => {
+    //   console.log(this.name + ' has lived in ' + city);
+    // });
+  }
+};
+
+console.log(user.printPlacesLived());
+
+// Challenge
+
+var multiplier = {
+  // numbers - array of numbers to multiply
+  // multiplyBy - single number
+  // multiply - return a new array where the numbers have been mutlplied
+  numbers: [5, 10, 12, 15, 245],
+  multiplyBy: 3.4,
+  multiply: function multiply() {
+    var _this2 = this;
+
+    return this.numbers.map(function (number) {
+      return number * _this2.multiplyBy;
     });
   }
 };
 
-user.printPlacesLived();
+console.log(multiplier.multiply());
--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -16,19 +16,32 @@ const user = {
   // printPlacesLived: function () {
   // new syntax
   printPlacesLived() {
-    console.log(this.name);
-    console.log(this.cities);
-
-    this.cities.forEach(function (city){
+    return this.cities.map((city) => this.name + ' has lived in ' + city);
+    // this.cities.forEach(function (city){
       // this is no longer bound in the loop!
       // usually we do: const that = this above
       // console.log(this.name + ' has lived in ' + city);
-    });
+    // });
     // arrow function does work
-    this.cities.forEach((city) => {
-      console.log(this.name + ' has lived in ' + city);
-    });
+    // this.cities.forEach((city) => {
+    //   console.log(this.name + ' has lived in ' + city);
+    // });
   }
 };
 
-user.printPlacesLived();
+console.log(user.printPlacesLived());
+
+// Challenge
+
+const multiplier = {
+  // numbers - array of numbers to multiply
+  // multiplyBy - single number
+  // multiply - return a new array where the numbers have been mutlplied
+  numbers: [5, 10, 12, 15, 245],
+  multiplyBy: 3.4,
+  multiply() {
+    return this.numbers.map((number) => number * this.multiplyBy);
+  }
+};
+
+console.log(multiplier.multiply());
