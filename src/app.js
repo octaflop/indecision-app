07116945eac4c8248646ab7e4936b18fd548eddb
Faris Chebib <faris@theluckybead.com>
2017-12-27 11:10:23 -0700

    Add 3rd party example

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
-// import thing, { square, add } from './utils.js';
-//
-// console.log('app.js is still running!!!');
-// console.log(square(4.5));
-// console.log(add(4, 100));
-// console.log(thing(4, 100));
+// install → import → use
+import validator from 'validator';
 
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log('isSenior', isSenior(65));
-console.log('isSenior', isSenior(68));
-console.log('isAdult', isAdult(17));
-console.log('isAdult', isAdult(21));
-console.log('canDrink', canDrink(19));
-console.log('canDrink', canDrink(23));
+console.log(validator.isEmail('f'));
+console.log(validator.isEmail('f@example.com'));
+console.log(validator.isEmail('f@example.cöm'));
---
+++ src/person.js
@@ -0,0 +1,9 @@
+
+const isAdult = (x) => x >= 18;
+const canDrink = (x) => x >= 21;
+
+const isSenior = (x) => x >= 68;
+export default isSenior;
+
+
+export { isAdult, canDrink };
