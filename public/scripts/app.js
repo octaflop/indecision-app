b6ca6609155eed81d7f7daf3b18a04c930aad26b
Faris Chebib <faris@theluckybead.com>
2017-12-26 17:15:37 -0700

    Show regenerate component stuff

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,57 +1,87 @@
 'use strict';
 
-// arguments object - no longer bound with arrow
+console.log('App.js is running');
 
-var add = function add(a, b) {
-  // referenceError
-  // console.log(arguments);
-  return a + b;
+var app = {
+  title: 'Some Title',
+  subtitle: 'Some subtitle',
+  options: ['First', 'Second']
+
+  // JSX - Javascript XML
+};var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options && app.options.length > 0 ? 'Here are your options' : 'No Options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement('li', null),
+    React.createElement('li', null)
+  )
+);
+
+var count = 0;
+var addOne = function addOne() {
+  count++;
+  console.log('addOne', count);
+  renderCounterApp();
+};
+var minusOne = function minusOne() {
+  count--;
+  console.log('minusOne');
+  renderCounterApp();
+};
+var reset = function reset() {
+  count = 0;
+  console.log('reset');
+  renderCounterApp();
 };
 
-console.log(add(55, 1, 1001));
+var appRoot = document.getElementById('app');
 
-// this keyword is no longer bound
+// ReactDOM.render(template, appRoot);
 
-var user = {
-  name: 'Faris',
-  cities: ['Atlanta', 'New York', 'Vancouver', 'Salt Lake City'],
-  // printPlacesLived: function () {
-  // new syntax
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
-    // this.cities.forEach(function (city){
-    // this is no longer bound in the loop!
-    // usually we do: const that = this above
-    // console.log(this.name + ' has lived in ' + city);
-    // });
-    // arrow function does work
-    // this.cities.forEach((city) => {
-    //   console.log(this.name + ' has lived in ' + city);
-    // });
-  }
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'reset'
+    )
+  );
+  ReactDOM.render(templateTwo, appRoot);
 };
 
-console.log(user.printPlacesLived());
-
-// Challenge
-
-var multiplier = {
-  // numbers - array of numbers to multiply
-  // multiplyBy - single number
-  // multiply - return a new array where the numbers have been mutlplied
-  numbers: [5, 10, 12, 15, 245],
-  multiplyBy: 3.4,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
-  }
-};
-
-console.log(multiplier.multiply());
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -22,30 +22,38 @@ const template = (
   </div>
 );
 
-const userName = 'Faris';
-const userAge = 29.5;
-const userLocation = 'Salt Lake City';
 
-const user = {
-  name: 'Faris',
-  age: 29.5,
-  location: 'SLC, UT'
-}
+let count = 0;
+const addOne = () => {
+  count++;
+  console.log('addOne', count);
+  renderCounterApp();
+};
+const minusOne = () => {
+  count--;
+  console.log('minusOne')
+  renderCounterApp();
+};
+const reset = () => {
+  count = 0;
+  console.log('reset')
+  renderCounterApp();
+};
 
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  }
-}
+const appRoot = document.getElementById('app');
 
-const templateTwo = (
-  <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
-  </div>
-);
-var appRoot = document.getElementById('app');
+// ReactDOM.render(template, appRoot);
 
-ReactDOM.render(template, appRoot);
-// ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
