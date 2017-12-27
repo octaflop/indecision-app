7f10904dd5b0fbabc2992b42bce0eefa7a47855a
Faris Chebib <faris@theluckybead.com>
2017-12-27 16:16:23 -0700

    Add modal style and other tweaks

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,12 +5,15 @@ const OptionModal = (props) => (
   <Modal
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
+    closeTimeoutMS={300}
     ariaHideApp={false}
     onRequestClose={props.handleClearSelectedOption}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
     <button
+      className="button"
       onClick={props.handleClearSelectedOption}>Ok</button>
   </Modal>
 );
