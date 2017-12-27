9dd42d9676090ad3f55af228d1e899502e611c17
Faris Chebib <faris@theluckybead.com>
2017-12-27 12:19:00 -0700

    Example of newSyntax

--- src/app.js
+++ src/app.js
@@ -4,3 +4,24 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+// old syntax
+class OldSyntax {
+  constructor() {
+    this.name = 'Mike';
+  }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+
+// New Syntax
+
+class NewSyntax {
+  name = 'New';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
